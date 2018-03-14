428d654921970c9c207893762cbcf59e691f6e02
Ninos <yomon@mcmaster.ca>
2018-03-13 21:50:46 -0400

    Converted reviews to Map data structure with minimal functionality

--- final_Project/src/final_Project/Reviews.java
+++ final_Project/src/final_Project/Reviews.java
@@ -1,51 +1,49 @@
 package final_Project;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Reviews {
 	private static Scanner sc;
 
-	//Creates the reviews database if not already found.
-	public static void fileCheck() throws IOException {
-		File file = new File("Reviews.txt");
+	//Creates the reviews database if not already found, loads the map.
+	public static Map<String, List<String>> initMapFromFile(String fileName) throws IOException {
+		File file = new File(fileName);
 		if (!file.exists()) {
+			System.out.println("File not found, creating file...");
 			file.createNewFile();
+			return null;
 		}
-	}
-	
-	//Find a contractor based on license number in the reviews file, then returns
-	//their reviews data in the line including license number. Line must not 
-	//contain any space, use commas for separation only in reviews.txt. Returns
-	// null if not found.
-	public static String findContractor(String licenseNumber) throws IOException {
-		fileCheck();
-		File file = new File("Reviews.txt");
 		sc = new Scanner(file);
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
 		while (sc.hasNext()) {
 			String line = sc.next();
-			if (line.substring(0, 12).equals(licenseNumber)) {
-				return line;
-			}
+			String reviews = line.substring(13, line.length());
+			List<String> reviewsList = Arrays.asList(reviews.split(","));
+			map.put(line.substring(0, 12),reviewsList);
 		}
-		return null;
+		return map;
 	}
 	
 	//If a contractor is found, returns the average of their reviews, being the 
 	//last number in the line.
-	public static String avgOfContractor(String licenseNumber) throws IOException {
-		String reviews = findContractor(licenseNumber);
-		if (reviews != null) {
-			reviews = reviews.substring(13, reviews.length());
-			List<String> reviewsList = Arrays.asList(reviews.split(","));
-			return reviewsList.get(reviewsList.size() - 1);
+	public static String avgOfContractor(String licenseNumber, Map <String, List<String>> map) {
+		if (map.containsKey(licenseNumber)) {
+		int size = map.get(licenseNumber).size();
+		return map.get(licenseNumber).get(size - 1);
 		}
-		else return null;
+		return null;
 	}
 	
 	public static void main(String[] args) throws IOException {
+		Map<String, List<String>> map = initMapFromFile("Reviews.txt");
+		String avg = avgOfContractor("ABCDEFGHIJKL", map);
+		System.out.println(avg);
 	}
 }
